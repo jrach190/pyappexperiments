bb39fa035955ee540ce07c46040aea170875cdb9
Jonathan Rach <jrach190@gmail.com>
2016-03-08 10:52:15 -0500

    Credit to source for experimentation code/process

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+"""Code from http://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-22972 """
+"""Beginning basic web app dev with python"""
+
+
 from flask import Flask, render_template
 
 
